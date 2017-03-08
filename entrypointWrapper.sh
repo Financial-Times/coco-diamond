6d6ba35963ab6bb847f034902bad5a009ee7c93a
Sorin Buliarca <Sorin.Buliarca@iquestgroup.com>
2017-03-08 14:29:24 +0200

    Added environment as prefix.

--- entrypointWrapper.sh
+++ entrypointWrapper.sh
@@ -2,6 +2,7 @@
 
 TEMP=. read -r -a HOSTNAME_SLICED <<< "$NODE_NAME"
 export DOCKER_HOSTNAME="${ENVIRONMENT}.${HOSTNAME_SLICED}"
+export PREFIX="${ENVIRONMENT}.servers"
 
 echo "DOCKER_HOSTNAME=${DOCKER_HOSTNAME}"
 ./entrypoint.sh
