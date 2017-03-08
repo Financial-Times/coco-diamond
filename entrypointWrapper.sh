e4469b13ec386eadf114dea7211e5d5929ff541b
Sorin Buliarca <Sorin.Buliarca@iquestgroup.com>
2017-03-08 15:32:17 +0200

    Do not compose the prefix

--- entrypointWrapper.sh
+++ entrypointWrapper.sh
@@ -2,7 +2,6 @@
 
 read -r -d "." HOSTNAME_SLICED <<< "$NODE_NAME"
 export DOCKER_HOSTNAME="${ENVIRONMENT}.${HOSTNAME_SLICED}"
-export PREFIX="${ENVIRONMENT}.servers"
 
 echo "DOCKER_HOSTNAME=${DOCKER_HOSTNAME}"
 ./entrypoint.sh
